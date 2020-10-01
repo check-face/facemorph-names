af32f0e5f562e94451593c57a6d623c85d35b965
Oliver Coad <oliver.coad@gmail.com>
2020-10-02 07:31:14 +1000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<!-- <script>
+	import ImageGrid from './ImageGrid.svelte'
+
+	const chars = 'abcdefghijklmnopqrstuvwxyz'
+
+	let currentVal = 0;
+	let currentPrefix = "";
+	$: {
+		let v = "";
+		let c = currentVal - 1;
+		if(c == 0) { v = chars[0] }
+		while(c > 0) {
+			v = chars[(c % chars.length)] + v
+			c = Math.floor(c / chars.length)
+		}
+		currentPrefix = v;
+	}
+
+	let values = [ ];
+	$: {
+		values = [ ];
+		for(let i = 0; i < chars.length; i++) {
+			values.push(currentPrefix + chars[i]);
+		}
+	}
+</script>
+
+<h1>Current prefix: {currentPrefix}</h1>
+<div style="float: right">
+	<button disabled={currentVal <= 0} on:click={() => currentVal -= 1}>Prev</button>
+	<button on:click={() => currentVal += 1}>Next</button>
+</div>
+
+
+<ImageGrid {values} ></ImageGrid>
+
+<style>
+h1 {
+	display: inline;
+	padding-right: 1em;
+	font-family: sans-serif;
+}
+</style> -->
+
+<script>
+import ImageGrid from './ImageGrid.svelte'
+// import values from './greetings.js'
+import babynames from './babynames.js'
+
+const distinct = (value, index, self) => {
+	return self.indexOf(value) === index;
+}
+
+let values = babynames.slice(0, 100).map(name => name.toLowerCase())
+</script>
+
+<ImageGrid values={values}></ImageGrid>
---
+++ src/ImageGrid.svelte
@@ -0,0 +1,46 @@
+<script>
+export let values = [ "asdf", "qwerty" ];
+export let dim = 200;
+</script>
+
+
+<div id="grid" style="--dim: {dim}px">
+	{#each values as value (value)}
+	<div>
+		<img width={dim} height={dim} src="https://api.checkface.ml/api/face/?dim={dim}&value={encodeURIComponent(value)}" alt={value}>
+		<p>{value}</p>
+	</div>
+	{/each}
+</div>
+
+<style>
+#grid {
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(var(--dim), 1fr));
+	/* grid-template-columns: repeat(auto-fit, minmax(auto, 150px)); */
+}
+
+#grid div {
+	position: relative;
+}
+
+img {
+	width: 100%;
+	height: auto;
+	display: block;
+}
+
+p {
+	position: absolute;
+	bottom: 0;
+	margin: 0;
+	color: white;
+	font-size: 2em;
+	font-family: sans-serif;
+	right: 10px;
+
+	color: rgb(255, 255, 255); 
+	text-shadow: 2px 2px black;
+
+}
+</style>
